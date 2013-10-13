0dd3ac24ac0f905f71173b1ed84c7dd8bd9fd09e
Albert Gazizov <deeper4k@gmail.com>
2013-10-13 20:39:42 +0400

    Start refactoring ChildrenCaster

--- lib/aggregate_builder/children_caster.rb
+++ lib/aggregate_builder/children_caster.rb
@@ -6,6 +6,7 @@ module AggregateBuilder
       @builder       = builder
       @attributes    = attributes
       @entity        = entity
+      @errors_notifier = ErrorsNotifier.new(@builder_rules)
     end
 
     def cast
@@ -19,55 +20,53 @@ module AggregateBuilder
 
           if children_or_child.respond_to?(:each)
             if !(association_attributes.is_a?(Array) && association_attributes.all? {|a| a.is_a?(Hash)})
-              if @builder_rules.warn_level?
-                p "WARNING: You should provide array of hashes for #{child_metadata.child_name}"
-              elsif @builder_rules.error_level?
-                raise Errors::AssociationParamsError, "You should provide array of hashes for #{child_metadata.child_name}"
-              else
-                next
-              end
+              @errors_notifier.notify_nested_array_type_error(child_metadata.child_name)
+              next
             end
-            association_attributes.each do |attrs|
-              child = find_child(children_or_child, attrs)
-
-              if should_build?(child_metadata, attrs)
-                if child && should_delete?(child_metadata, attrs)
-                  @entity.send(child_metadata.child_name).delete_if do |child_entity|
-                    child_entity == child
-                  end
-                else
-                  builder = get_builder(child_metadata)
-                  @entity.send(child_metadata.child_name) << builder.build(child, attrs)
-                end
-              end
-            end
+            cast_array(children_or_child, child_metadata, association_attributes)
           else
             if !association_attributes.is_a?(Hash)
-              if @builder_rules.warn_level?
-                p "WARNING: You should provide hash for #{child_metadata.child_name}"
-              elsif @builder_rules.error_level?
-                raise Errors::AssociationParamsError, "You should provide hash for #{child_metadata.child_name}"
-              else
-                next
-              end
+              @errors_notifier.notify_nested_hash_type_error(child_metadata.child_name)
+              next
             end
-            if should_build?(child_metadata, association_attributes)
-              if should_delete?(child_metadata, association_attributes)
-                @entity.send("#{child_metadata.child_name}=", nil)
-              else
-                builder = get_builder(child_metadata)
-                @entity.send(
-                  "#{child_metadata.child_name}=",
-                  builder.build(children_or_child, association_attributes)
-                )
-              end
-            end
+            cast_hash(children_or_child, child_metadata, association_attributes)
           end
         end
       end
     end
 
     private
+
+    def cast_array(children, child_metadata, association_attributes)
+      association_attributes.each do |attrs|
+        child = find_child(children, attrs)
+
+        if should_build?(child_metadata, attrs)
+          if child && should_delete?(child_metadata, attrs)
+            @entity.send(child_metadata.child_name).delete_if do |child_entity|
+              child_entity == child
+            end
+          else
+            builder = get_builder(child_metadata)
+            @entity.send(child_metadata.child_name) << builder.build(child, attrs)
+          end
+        end
+      end
+    end
+
+    def cast_hash(child, child_metadata, association_attributes)
+      if should_build?(child_metadata, association_attributes)
+        if should_delete?(child_metadata, association_attributes)
+          @entity.send("#{child_metadata.child_name}=", nil)
+        else
+          builder = get_builder(child_metadata)
+          @entity.send(
+            "#{child_metadata.child_name}=",
+            builder.build(child, association_attributes)
+          )
+        end
+      end
+    end
 
     def get_builder(child_metadata)
       if child_metadata.builder.is_a?(Class)
--- lib/aggregate_builder/errors_notifier.rb
+++ lib/aggregate_builder/errors_notifier.rb
@@ -5,21 +5,46 @@ module AggregateBuilder
     end
 
     def notify_missing_attribute(field, builder)
+      message = "Required field #{field.field_name} is missing for #{builder.class} builder"
       if @builder_rules.warn_level?
-        p "Warning: Required field #{field.field_name} is missing for #{builder.class} builder"
+        p "Warning: #{mesage}"
       elsif @builder_rules.error_level?
-        raise Errors::RequireAttributeMissingError, "Required field #{field.field_name} is missing for #{builder.class} builder"
+        raise Errors::RequireAttributeMissingError, message
+      elsif @builder_fules.silent_level?
+        nil
       end
     end
 
     def notify_casting_error(exception)
-      if @builder_rules.silent_level?
-        nil
-      elsif @builder_rules.warn_level?
+      if @builder_rules.warn_level?
         p exception.message
         nil
       elsif @builder_rules.error_level?
         raise exception
+      elsif @builder_fules.silent_level?
+        nil
+      end
+    end
+
+    def notify_nested_array_type_error(field_name)
+      message = "You should provide array of hashes for #{field_name}"
+      if @builder_rules.warn_level?
+        p "WARNING: #{message}"
+      elsif @builder_rules.error_level?
+        raise Errors::AssociationParamsError, message
+      elsif @builder_fules.silent_level?
+        nil
+      end
+    end
+
+    def notify_nested_hash_type_error(field_name)
+      message = "You should provide hash for #{field_name}"
+      if @builder_rules.warn_level?
+        p "WARNING: #{message}"
+      elsif @builder_rules.error_level?
+        raise Errors::AssociationParamsError, message
+      elsif @builder_fules.silent_level?
+        nil
       end
     end
 
