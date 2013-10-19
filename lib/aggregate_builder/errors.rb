1d4487a71a20c6bf007469f9f9870ba248172118
Albert Gazizov <deeper4k@gmail.com>
2013-10-19 20:47:02 +0400

    Clean up

--- lib/aggregate_builder/errors.rb
+++ lib/aggregate_builder/errors.rb
@@ -1,12 +1,10 @@
 module AggregateBuilder
   module Errors
-    class BuildError < StandardError; end
+    class AggregateBuilderError              < StandardError; end
 
-    class UndefinedRootClassError < BuildError; end
-    class UnexpectedAttribute < BuildError; end
-    class TypeCastingError < BuildError; end
-    class AssociationParamsError < BuildError; end
-    class FieldNotDefinedError < BuildError; end
-    class ImmutableFieldError < BuildError; end
+    class UndefinedRootClassError < AggregateBuilderError; end
+    class UnexpectedAttribute     < AggregateBuilderError; end
+    class TypeCastingError        < AggregateBuilderError; end
+    class ImmutableFieldError     < AggregateBuilderError; end
   end
 end
