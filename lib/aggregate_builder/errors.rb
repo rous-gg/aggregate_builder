252a881bcaf283c5bdaef90fd20116f86ea59216
Albert Gazizov <deeper4k@gmail.com>
2013-11-29 11:22:48 +0400

    Exceptions hierarhy

--- lib/aggregate_builder/errors.rb
+++ lib/aggregate_builder/errors.rb
@@ -1,9 +1,10 @@
 module AggregateBuilder
   module Errors
     class AggregateBuilderError   < StandardError; end
+    class AttributesError         < AggregateBuilderError; end
     class UndefinedRootClassError < AggregateBuilderError; end
-    class UnexpectedAttribute     < AggregateBuilderError; end
-    class TypeCastingError        < AggregateBuilderError; end
-    class ImmutableFieldError     < AggregateBuilderError; end
+    class UnexpectedAttribute     < AttributesError; end
+    class TypeCastingError        < AttributesError; end
+    class ImmutableFieldError     < AttributesError; end
   end
 end
